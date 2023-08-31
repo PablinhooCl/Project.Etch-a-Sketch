07a3afc204cc97b9816ae35941296ca2b75c3a2b
pablinhoocl <xpabliito@gmail.com>
2023-08-30 17:49:21 -0700

    project finished, ui 100%, functions 100%, extra credit 2/2

--- code.js
+++ code.js
@@ -1,23 +1,60 @@
-// // set f on start
-//     window.onload = function(){
-//         setGrid();
-//     };
+let click = true
+let color= 'black';
+function boardSize(size){
 
+if (size>=2 && size<=100){
 
 let board= document.querySelector('.board');
-    board.style.gridTemplateColumns= 'repeat(16, 1fr)';
-    board.style.gridTemplateRows= 'repeat(16, 1fr)';
+    board.style.gridTemplateColumns= `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows= `repeat(${size}, 1fr)`;
 
-let div = document.createElement("div");
-for(let i= 0; i<256; i++){
-    
-    document.getElementById("main").appendChild(div);
-    div.classList.add("draw")
-    ;
+  let sizeSquare= size*size;
+  
+    for(let i= 0; i<sizeSquare; i++){
+    let div = document.createElement("div");
+    board.appendChild(div);
+    board.insertAdjacentElement('beforeend', div);
+    div.addEventListener("mouseover", changeColor);
+    div.style.backgroundColor= 'white';
+    };
+  } else{
+
+      alert('ERROR, try numbers beetwen 2 and 100 it may take some time to load')
+
+    } 
   }
-  pen= document.getElementsByClassName('draw');
-  pen.addEventListener(
-    "mouseenter",
-    (event) => {
-    div.style.backgroundColor= 'blue'    
-    });
+
+boardSize(16);
+  function resetBoard(){
+    let board= document.querySelector(".board");
+    let pixel= board.querySelectorAll("div");
+    pixel.forEach((div) => (div.style.backgroundColor= 'white'));
+    blackGradient= 240;
+  };
+  function blackBoard(){
+    let board= document.querySelector(".board");
+    let pixel= board.querySelectorAll("div");
+    pixel.forEach((div) => (div.style.backgroundColor= 'black'));
+  }
+
+
+  function colorChanger(input){
+    color=input
+  };
+let blackGradient= 240
+let randomColor= `hsl(${Math.random() *360},100%, 50%)` ;
+console.log(blackGradient);
+  function changeColor(){
+    if (click){
+    if (color === "random"){
+      this.style.backgroundColor=`hsl(${Math.random() *360},100%, 50%)` ;}
+      else if(color==="black"){
+      this.style.backgroundColor = `rgb(${blackGradient}, ${blackGradient}, ${blackGradient})`;
+      console.log(blackGradient);
+      blackGradient-= 24;
+      
+      
+     }
+    else{
+      this.style.backgroundColor= color}} }
+   
